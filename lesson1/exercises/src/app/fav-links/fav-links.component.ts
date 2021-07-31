eb36ea9792db6c6777895485af985c00f070c9be
Jacob Walton <jawalton6616@gmail.com>
2021-07-31 13:32:07 -0500

    completed part 2 & 3

---
+++ lesson1/exercises/src/app/fav-links/fav-links.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "fav-links",
+  templateUrl: "./fav-links.component.html",
+  styleUrls: ["./fav-links.component.css"],
+})
+export class FavLinksComponent implements OnInit {
+  constructor() {}
+  favLinks: string[] = ["google.com", "tidal.com"];
+  ngOnInit() {}
+}
